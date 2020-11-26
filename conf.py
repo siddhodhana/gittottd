393c78883b801d135e92717650ad0ec46db8a6a4
siddhodhana <51401860+siddhodhana@users.noreply.github.com>
2020-11-26 15:29:39 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -17,9 +17,9 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'E-commerce project using Java'
-copyright = '2020, Harideep'
-author = 'Harideep'
+project = 'lms project'
+copyright = '2020, siddhodhana'
+author = 'siddodhana'
 
 # The full version, including alpha/beta/rc tags
 release = ''
